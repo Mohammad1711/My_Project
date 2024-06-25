ef6f8cbf503038a8f557b25b8bb7f8da81bb0156
mohamed <mohammad.abu.rabia.1993@gmail.com>
2024-06-25 22:07:41 +0300

    streamlit edit

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ import altair as alt
 file_path = './vehicles_us.csv'
 df = pd.read_csv(file_path)
 
-st.header('This is a header with a divider', divider='rainbow')
+st.header('This is a header with a divider')
 st.header('this is test')
