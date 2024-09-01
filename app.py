75028a39236d11ac74e1b212252dfc43016aaeaf
mohamed <mohammad.abu.rabia.1993@gmail.com>
2024-09-01 07:55:57 +0300

    Changes to be committed: 	modified:   app.py 	renamed:    notebooks/mynotebook.ipynb -> notebooks/EDA.ipynb

--- app.py
+++ app.py
@@ -3,8 +3,31 @@ import pandas as pd
 import plotly.express as px
 import altair as alt
 
-file_path = './vehicles_us.csv'
-df = pd.read_csv(file_path)
+df = pd.read_csv('vehicles_us.csv')  
+st.header('Car Advertisement Data Analysis')
+st.subheader('Distribution of Car Prices')
+fig = px.histogram(df, x='price', title='Price Distribution')
+st.plotly_chart(fig)
 
-st.header('This is a header with a divider')
-st.header('this is test')
+st.subheader('Distribution of Car Prices')
+fig = px.histogram(df, x='price', title='Price Distribution')
+st.plotly_chart(fig)
+
+st.subheader('Price vs Model Year')
+fig2 = px.scatter(df, x='model_year', y='price', color='condition', title='Price vs Model Year by Condition')
+st.plotly_chart(fig2)
+
+
+low_mileage = st.checkbox('Show only low mileage cars (<= 50,000 miles)')
+
+if low_mileage:
+    df = df[df['odometer'] <= 50000]
+
+
+st.subheader('Distribution of Car Prices')
+fig = px.histogram(df, x='price', title='Price Distribution')
+st.plotly_chart(fig)
+
+st.subheader('Price vs Model Year')
+fig2 = px.scatter(df, x='model_year', y='price', color='condition', title='Price vs Model Year by Condition')
+st.plotly_chart(fig2)
